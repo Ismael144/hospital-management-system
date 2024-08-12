316b2887402d5df1d5d0bdd05701f1fde442379e
Ismael <swalehismael144@gmail.com>
2024-08-12 15:44:25 +0300

    Removed PharmacyInventory from pharmacy

--- pharmacy/views.py
+++ pharmacy/views.py
@@ -1,12 +1,19 @@
+# views.py
+
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required, permission_required
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
-from .models import Medication, Prescription, Dispensation, PharmacyInventory
+from .models import Medication, Prescription, Dispensation
 from .forms import MedicationForm, PrescriptionForm, DispensationForm, PharmacyInventoryForm
 from django.views import View
+from activities.models import Activity
+
+# Utility function to log activity
+def log_activity(user, action, description):
+    Activity.objects.create(user=user, action=action, description=description)
 
 # Medication Views
 
@@ -32,6 +39,11 @@ class MedicationCreateView(SuccessMessageMixin, CreateView):
     success_url = reverse_lazy('medication_list')
     success_message = "Medication created successfully"
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        log_activity(self.request.user, 'Create Medication', f'Medication {self.object.name} was created.')
+        return response
+
 
 @method_decorator([login_required, permission_required('pharmacy.change_medication', raise_exception=True)], name='dispatch')
 class MedicationUpdateView(SuccessMessageMixin, UpdateView):
@@ -41,6 +53,11 @@ class MedicationUpdateView(SuccessMessageMixin, UpdateView):
     success_url = reverse_lazy('medication_list')
     success_message = "Medication updated successfully"
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        log_activity(self.request.user, 'Update Medication', f'Medication {self.object.name} was updated.')
+        return response
+
 
 @method_decorator([login_required, permission_required('pharmacy.delete_medication', raise_exception=True)], name='dispatch')
 class MedicationDeleteView(SuccessMessageMixin, DeleteView):
@@ -48,6 +65,11 @@ class MedicationDeleteView(SuccessMessageMixin, DeleteView):
     template_name = 'pharmacy/medication_confirm_delete.html'
     success_url = reverse_lazy('medication_list')
     success_message = "Medication deleted successfully"
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        log_activity(request.user, 'Delete Medication', f'Medication {self.object.name} was deleted.')
+        return super().delete(request, *args, **kwargs)
 
 # Prescription Views
 
@@ -73,6 +95,11 @@ class PrescriptionCreateView(SuccessMessageMixin, CreateView):
     success_url = reverse_lazy('prescription_list')
     success_message = "Prescription created successfully"
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        log_activity(self.request.user, 'Create Prescription', f'Prescription {self.object.id} was created.')
+        return response
+
 
 @method_decorator([login_required, permission_required('pharmacy.change_prescription', raise_exception=True)], name='dispatch')
 class PrescriptionUpdateView(SuccessMessageMixin, UpdateView):
@@ -82,6 +109,11 @@ class PrescriptionUpdateView(SuccessMessageMixin, UpdateView):
     success_url = reverse_lazy('prescription_list')
     success_message = "Prescription updated successfully"
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        log_activity(self.request.user, 'Update Prescription', f'Prescription {self.object.id} was updated.')
+        return response
+
 
 @method_decorator([login_required, permission_required('pharmacy.delete_prescription', raise_exception=True)], name='dispatch')
 class PrescriptionDeleteView(SuccessMessageMixin, DeleteView):
@@ -89,6 +121,11 @@ class PrescriptionDeleteView(SuccessMessageMixin, DeleteView):
     template_name = 'pharmacy/prescription_confirm_delete.html'
     success_url = reverse_lazy('prescription_list')
     success_message = "Prescription deleted successfully"
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        log_activity(request.user, 'Delete Prescription', f'Prescription {self.object.id} was deleted.')
+        return super().delete(request, *args, **kwargs)
 
 # Dispensation Views
 
@@ -114,6 +151,11 @@ class DispensationCreateView(SuccessMessageMixin, CreateView):
     success_url = reverse_lazy('dispensation_list')
     success_message = "Medication dispensed successfully"
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        log_activity(self.request.user, 'Create Dispensation', f'Dispensation {self.object.id} was created.')
+        return response
+
 
 @method_decorator([login_required, permission_required('pharmacy.change_dispensation', raise_exception=True)], name='dispatch')
 class DispensationUpdateView(SuccessMessageMixin, UpdateView):
@@ -123,6 +165,11 @@ class DispensationUpdateView(SuccessMessageMixin, UpdateView):
     success_url = reverse_lazy('dispensation_list')
     success_message = "Dispensation updated successfully"
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        log_activity(self.request.user, 'Update Dispensation', f'Dispensation {self.object.id} was updated.')
+        return response
+
 
 @method_decorator([login_required, permission_required('pharmacy.delete_dispensation', raise_exception=True)], name='dispatch')
 class DispensationDeleteView(SuccessMessageMixin, DeleteView):
@@ -130,3 +177,8 @@ class DispensationDeleteView(SuccessMessageMixin, DeleteView):
     template_name = 'pharmacy/dispensation_confirm_delete.html'
     success_url = reverse_lazy('dispensation_list')
     success_message = "Dispensation deleted successfully"
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        log_activity(request.user, 'Delete Dispensation', f'Dispensation {self.object.id} was deleted.')
+        return super().delete(request, *args, **kwargs)
