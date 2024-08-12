54a73fe2e03b1718c4e3d705737a426a72fb5609
Ismael <swalehismael144@gmail.com>
2024-08-12 14:52:37 +0300

    Working on Appointment model and views

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/bash
 
-waitress-serve --listen=127.0.0.1:5000 hms:wsgi.application
+USE_WAITRESS="waitress"
+
+if [ $1 == "$USE_WAITRESS" ]; then
+    waitress-serve --listen=127.0.0.1:5000 hms:wsgi.application
+else
+    python manage.py runserver&
+    ./run.sh&
+fi
